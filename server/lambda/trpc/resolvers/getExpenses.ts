009669fd551e35eed59d77343cbcbd59ba683a4f
Daniel Malcolm <daniel.malcolm@lego.com>
2023-03-08 20:03:35 +0000

    remove example response

--- server/lambda/trpc/resolvers/getExpenses.ts
+++ server/lambda/trpc/resolvers/getExpenses.ts
@@ -19,30 +19,9 @@ export const getExpenses = async () => {
     TableName: process.env.TABLE_NAME,
   };
 
-  const exampleData = {
-    Items: [
-      {
-        PK: { S: "Expense#1" },
-        SK: { S: "Expense#1" },
-        value: { N: "1" },
-        description: { S: "test" },
-        date: { S: "2023-03-10" },
-      },
-      {
-        PK: { S: "Expense#2" },
-        SK: { S: "Expense#2" },
-        value: { N: "2" },
-        description: { S: "test2" },
-        date: { S: "2023-03-10" },
-      }
-    ]
-  };
   try {
     const data = await client.send(new ScanCommand(params));
     console.log("Success", data);
-    if(!data.Items) {
-      return exampleData.Items;
-    }
     return data.Items;;
   } catch (err) {
     console.log("Error", err);
