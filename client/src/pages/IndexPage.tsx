5a729fc1a546a63a290644a249db59a8528b6d87
Daniel Malcolm <daniel.malcolm@lego.com>
2023-03-08 21:46:35 +0000

    render expense items in table

--- client/src/pages/IndexPage.tsx
+++ client/src/pages/IndexPage.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { trpc } from '../utils/trpc';
+import { trpc } from "../utils/trpc";
 
 import ExpenseForm from "./ExpenseForm";
 
 export default function IndexPage() {
   const expenses = trpc.getExpenses.useQuery();
-  if (expenses.status === 'loading') {
+  if (expenses.status === "loading") {
     return <div>Loading...</div>;
   }
-  if (expenses.status === 'error') {
+  if (expenses.status === "error") {
     return <div>Error: {expenses.error.message}</div>;
   }
   if (!expenses.data || expenses.data.length === 0) {
@@ -19,12 +19,38 @@ export default function IndexPage() {
   const sortedExpenses = expenses.data.sort((a, b) => {
     return new Date(a.date).getTime() - new Date(b.date).getTime();
   });
+  const totalThisMonth = sortedExpenses.reduce((acc, expense) => {
+    const date = new Date(expense.date);
+    const now = new Date();
+    if (date.getMonth() === now.getMonth()) {
+      return acc + parseFloat(expense.amount);
+    }
+    return acc;
+  }, 0);
+
   return (
     <div>
       <h1>Expenses</h1>
-      <pre>{JSON.stringify(sortedExpenses, null, 2)}</pre>
-      <ExpenseForm onSubmit={refetch}/>
+      <ExpenseForm onSubmit={refetch} />
+      <p>Total this month: {totalThisMonth}</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Amount</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedExpenses.map((expense) => (
+            <tr key={expense.id}>
+              <td>{expense.description}</td>
+              <td>{expense.amount}</td>
+              <td>{expense.date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
-
