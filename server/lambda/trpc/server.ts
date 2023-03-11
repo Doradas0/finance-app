4fac8fd4685e6e80c8995de8bacbbc47419f3f29
Daniel Malcolm <daniel.malcolm@lego.com>
2023-03-11 00:06:22 +0000

    refactor server to be transaction agnostic

--- server/lambda/trpc/server.ts
+++ server/lambda/trpc/server.ts
@@ -1,62 +1,84 @@
 import { inferAsyncReturnType, initTRPC } from "@trpc/server";
-import { getExpenses } from "./resolvers/getExpenses";
-import { createExpense } from "./resolvers/createExpense";
-import { getIncome } from "./resolvers/getIncome";
-import { createIncome } from "./resolvers/createIncome";
-import { z } from "zod";
-
 import {
   CreateAWSLambdaContextOptions,
   awsLambdaRequestHandler,
 } from "@trpc/server/adapters/aws-lambda";
+
 import { APIGatewayProxyEvent } from "aws-lambda";
+import { z } from "zod";
+
+const AWS = require("aws-sdk");
+import {
+  DynamoDBClient,
+  PutItemCommand,
+  QueryCommand,
+} from "@aws-sdk/client-dynamodb";
+
+const client = new DynamoDBClient({ region: "eu-west-1" });
 
 export const t = initTRPC.create();
 
 const helloRouter = t.router({
   hello: t.procedure.query(() => {
-    return "Hello World"
-  })
+    return "Hello World";
+  }),
 });
 
-const financeRouter = t.router({
-  createExpense: t.procedure
+const transactionRouter = t.router({
+  createTransaction: t.procedure
     .input(
       z.object({
         amount: z.number(),
         description: z.string(),
         date: z.string(),
         category: z.string(),
+        type: z.string(),
       })
     )
     .mutation(async ({ input }) => {
-      console.log("createExpense", input);
-      return createExpense(input);
+      const id = AWS.util.uuid.v4();
+      const params = {
+        TableName: process.env.TABLE_NAME,
+        Item: {
+          PK: { S: "User#1234" },
+          SK: { S: `Transaction#{id}` },
+          id: { S: id },
+          amount: { N: input.amount.toString() },
+          description: { S: input.description },
+          date: { S: input.date },
+          category: { S: input.category },
+          type: { S: input.type },
+        },
+      };
+      try {
+        const data = await client.send(new PutItemCommand(params));
+        console.log("Success", data);
+        return id;
+      } catch (err) {
+        console.log("Error", err);
+        return null;
+      }
     }),
-  getExpenses: t.procedure.query(async () => {
-    console.log("getExpenses");
-    return await getExpenses();
-  }),
-  createIncome: t.procedure
-    .input(
-      z.object({
-        amount: z.number(),
-        description: z.string(),
-        date: z.string(),
-        category: z.string(),
-      })
-    )
-    .mutation(async ({ input }) => {
-      console.log("createIncome", input);
-      return createIncome(input);
-    }),
-  getIncome: t.procedure.query(async () => {
-    console.log("getIncomes");
-    return await getIncome();
+  listTransactions: t.procedure.query(async () => {
+    const params = {
+      TableName: process.env.TABLE_NAME,
+      KeyConditionExpression: "PK = :pk",
+      ExpressionAttributeValues: {
+        ":pk": { S: "User#1234" },
+      },
+    };
+    try {
+      const data = await client.send(new QueryCommand(params));
+      console.log("Success", data);
+      return data;
+    } catch (err) {
+      console.log("Error", err);
+      return null;
+    }
   }),
 });
 
-const appRouter = t.mergeRouters(helloRouter, financeRouter);
+const appRouter = t.mergeRouters(helloRouter, transactionRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
